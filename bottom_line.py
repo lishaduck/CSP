61b67ec4243ceadf7f628377fc257f0926091a44
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:42 -0600

    fun health project

---
+++ bottom_line.py
@@ -0,0 +1,35 @@
+"""For health."""
+
+living = True
+right_amount = 3.2
+
+
+def death_by_overdose():
+    print("You died!")
+    is_living = False
+    return is_living
+
+
+def death_by_lacking():
+    print("You died!")
+    is_living = False
+    return is_living
+
+
+def live():
+    print("Good for you!")
+    is_living = True
+    return is_living
+
+
+while living:
+    amount = float(input("How much water do you have daily? (Liters): "))
+
+    if amount > 10:
+        living = death_by_overdose()
+
+    if amount < 10:
+        living = death_by_lacking()
+
+    if amount == 10:
+        living = live()
