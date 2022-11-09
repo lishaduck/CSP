5d3e7cc9d8e2c7fb0756d82f02eaeb60496e8fe0
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:41 -0600

    buggy_image part 2

--- turtles/buggy_image.py
+++ turtles/buggy_image.py
@@ -12,19 +12,38 @@ spider.circle(20)
 
 
 # initializes variables
-LEGS = 6
+LEGS = 4
+LEGS_PER_SIDE = 4
 LEG_LENGTH = 70
-DIRECTION = 380 / LEGS
+DIRECTION = 360 / 10
 spider.pensize(5)
 
 
 # this loop makes the legs, set the direction, and moves forward
-count = 0
-while count < LEGS:
-    spider.goto(0, 0)
-    spider.setheading(DIRECTION * count)
-    spider.forward(LEG_LENGTH)
-    count = count + 1
+for i in range(2):
+    count = 0
+    while count < LEGS:
+        spider.goto(0, 20)
+        if i == 1:
+            spider.setheading(DIRECTION * count + 45)
+        else:
+            spider.setheading(DIRECTION * count - 135)
+        spider.forward(LEG_LENGTH)
+        count = count + 1
+
+
+spider.penup()
+spider.goto(20, 20)
+spider.setheading(180)
+
+for i in range(2):
+    spider.fillcolor("White")
+    spider.begin_fill()
+    spider.circle(10)
+    spider.end_fill()
+    spider.begin_fill()
+    spider.right(135)
+    spider.forward(8)
 
 
 # shows the finished product and hides the turtle
