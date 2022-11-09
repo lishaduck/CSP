1939fd8758ab2c2856f7ae04a5cf2ef33d81372f
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:47 -0600

    unscramble_game

---
+++ art/functions/unscramble_game.py
@@ -0,0 +1,217 @@
+"""Program: unscramble_game.py - Play a fun game!
+
+"""
+
+
+import dataclasses
+import random
+
+
+# Create a list of words for the computer to choose from
+words = [
+    ".NET",
+    "gitignore",
+    "jupyter",
+    "markdown",
+    "NetLogo",
+    "javascript",
+    "TypeScript",
+    "flow",
+    "react",
+    "preact",
+    "JSON",
+    "CSS",
+    "scss",
+    "less",
+    "stylus",
+    "sass",
+    "styl",
+    "TypeScriptReact",
+    "HTML",
+    "python",
+    "swift",
+    "YAML",
+    "elm",
+    "Java",
+    "C",
+    "C++",
+    "C#",
+    "objective-C",
+    "objective-C++",
+    "elixir",
+    "Go",
+    "haxe",
+    "pug",
+    "jade",
+    "Lua",
+    "PHP",
+    "R",
+    "REST",
+    "GraphQL",
+    "CodeQL",
+    "ruby",
+    "rust",
+    "PowerShell",
+    "VHDL",
+    "Z80",
+    "WebAssembly",
+    "clojure",
+    "CoffeeScript",
+    "dart",
+    "diff",
+    "Docker",
+    "F#",
+    "groovy",
+    "HandleBars",
+    "HLSL",
+    "GLSL",
+    "ini",
+    "julia",
+    "LaTeX",
+    "log",
+    "make",
+    "Perl",
+    "Razor",
+    "reStructuredText",
+    "Shaderlab",
+    "shell",
+    "SQL",
+    "VisualBasic",
+    "WindowsBat",
+    "XML",
+    "mermaid",
+    "GeoJSON",
+    "TopoJSON",
+    "ASCII STL",
+    "ada",
+    "apacheconf",
+    "applescript",
+    "bash",
+    "console",
+    "erlang",
+    "fortran",
+    "haskell",
+    "markdownReact",
+    "postscript",
+    "PureScript",
+    "protobuf",
+    "scala",
+    "scheme",
+    "text",
+    "vim",
+    "ESM",
+    "commonjs",
+    "sarif",
+    "StarLogo",
+    "Vue",
+    "Angular",
+    "Ember",
+    "Glimmer",
+    "roc",
+]
+
+
+# credit where credit is due: https://stackoverflow.com/a/287944
+@dataclasses.dataclass
+class BColors:
+    """Non-comprehensive list of escape codes.
+
+    From:https://svn.blender.org/svnroot/bf-blender/trunk/blender/build_files/scons/tools/bcolors.py
+    """
+
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+# this function will take a word passed in as a parameter and return the same word scrambled
+def scramble(w):  # this defines the function
+    """"""
+    letters = list(w)  # this makes a list of the letters
+    random.shuffle(letters)  # this scrambles the letters
+    scramble_word = ""  # this initates a string
+    for i in letters:  # this is a for loop
+        scramble_word = scramble_word + i  # this makes the scrambled word
+    return scramble_word  # this makes the word accessable
+
+
+low_score = 0
+total = 0
+
+
+def instructions():
+    # print a statement for the directions
+    print(
+        f"""
+{BColors.HEADER}{BColors.BOLD}
+HOW TO PLAY:
+{BColors.ENDC}\n
+The game will print the {BColors.UNDERLINE}scrambled{BColors.ENDC} name of a programming language.
+Then, you will type in your answer.
+If it is correct, you'll be prompted to answer another.
+If your not, you'll be prompted to try again.
+To quit, type :exit.
+After 10 successes, you'll be given you score.
+{BColors.UNDERLINE}Lower{BColors.ENDC} is better.
+        """
+    )
+
+
+# Entrypoint
+def game():
+    global low_score
+    global total
+
+    score = 0
+    j = 0
+    answer = ""
+    while j < 10 and answer != ":exit":
+        # define a variable called secret_word and have it pick a random word from the list
+        secret_word = random.choice(words)
+        print("\n\n" + scramble(secret_word) + "\n")
+        answer = "rbt"  # short for `Roc Build Tool`,
+        # (continued:) rbt is pronounced 'ribbit', per https://github.com/roc-lang/rbt/
+        # Ask the user for their input to guess the word
+        while answer not in (secret_word, ":exit"):
+            answer = input("")
+
+            if answer not in (secret_word, ":exit"):
+                score = score + 1
+        j = j + 1
+    print("Score: ", score)
+    total = total + score
+    print("Total:", total)
+    low_score = max(low_score, score)
+    print("Low Score:", low_score)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if sys.argv[1:]:
+        is_playing = True
+        instructions()
+        while is_playing:
+            game()
+            still_playing = input("Want to play again? (y/n) ")
+            if still_playing not in ("Y", "y"):
+                is_playing = False
+        print("Bye!")
+    else:
+        print(__doc__)
+        want_to_play = input("Do you want to play? (y/n) ")
+        if want_to_play in ("Y", "y"):
+            is_playing = True
+
+            while is_playing:
+                game()
+                still_playing = input("Want to play again? (y/n) ")
+                if still_playing not in ("Y", "y"):
+                    is_playing = False
+            print("Bye!")
