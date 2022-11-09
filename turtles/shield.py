1a27259100e815969417e6ded17487e5cda37080
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:42 -0600

    make sheild shape
    (project v1)

---
+++ turtles/shield.py
@@ -0,0 +1,26 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+painter.shape("turtle")
+painter.speed(0)
+
+painter.penup()
+painter.goto(-200, 200)
+
+painter.pendown()
+painter.forward(400)
+painter.right(90)
+painter.forward(400)
+painter.penup()
+painter.goto(-200, -200)
+painter.pendown()
+painter.circle(200, 180)
+painter.penup()
+painter.goto(-200, -200)
+painter.pendown()
+painter.setheading(90)
+painter.forward(400)
+
+
+wn = trtl.Screen()
+wn.mainloop()
