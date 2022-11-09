3b7da5250f67d006f9463697e719f7589083ae3e
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 12:13:02 -0600

    finish catch-a-turtle
    and add the mod.s!
    
    Co-authored-by: LeParco1 <loparcov01@student.psdr3.org>

--- art/functions/catch_a_turtle.py
+++ art/functions/catch_a_turtle.py
@@ -10,20 +10,27 @@ import random as rand
 import turtle as trtl
 
 # --set the screen and turtle--
-
 wn = trtl.Screen()
+wn.bgcolor("black")
 spot = trtl.Turtle()
+spot.speed(10)
+score = 0
+
 
 # -----countdown variables-----
 font_setup = ("Arial", 20, "normal")
 timer = 30
 counter_interval = 1000  # 1000 represents 1 second
 timer_up = False
-score = 0
 
 
 # -----countdown writer-----
 counter = trtl.Turtle()
+counter.hideturtle()
+counter.pencolor("white")
+counter.penup()
+counter.goto(-50, 250)
+
 
 # -----game functions-----
 def countdown():
@@ -35,17 +42,20 @@ def countdown():
     global timer_up
     counter.clear()
     if timer <= 0:
-        counter.write("Time's Up", font=font_setup)
+        counter.write(f"Time's Up!    Score: {score}", font=font_setup)
         timer_up = True
+        spot.hideturtle()
     else:
-        counter.write("Timer: " + str(timer), font=font_setup)
+        counter.write(f"Timer: {timer}  Score: {score}", font=font_setup)
         timer -= 1
         counter.getscreen().ontimer(countdown, counter_interval)
 
 
 def spot_clicked(x: float, y: float):
-    # print("The spot was clicked!")
-    change_position()
+    if timer > 0:
+        global score
+        update_score()
+        change_position()
 
 
 def change_position():
@@ -56,26 +66,29 @@ def change_position():
     spot.pendown()
 
 
+def update_score():
+    global score
+    score = score + 1
+    print(score)
+
+
 def start_game():
     pass
 
 
 # Game Code
-spot_color = "pink"
-spot_size = 5
-spot_shape = "circle"
+spot_color = "light green"
+spot_size = rand.randint(3, 5)
+spot_shape = "turtle"
 spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
+spot.goto(-100, 0)
 
 
-# Timer code
-counter.hideturtle()
-counter.write(f"Time: {timer}")
-
-# ---------events---------
-wn.ontimer(countdown, counter_interval)
+# ---------events----------
 spot.onclick(spot_clicked)
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
 
 if __name__ == "__main__":
