0b5dcd6fbef635287f6b8032ce1c16605bd298dd
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:41 -0600

    add legs, format w/ black

--- turtles/ladybug_code.py
+++ turtles/ladybug_code.py
@@ -1,11 +1,41 @@
 """Program: ladybug_code.py - make a ladybug.
 """
+
+
 import turtle as trtl
+
 
 # create ladybug head
 ladybug = trtl.Turtle()
 ladybug.pensize(40)
 ladybug.circle(5)
+
+
+# initializes variables
+LEGS = 6
+LEGS_PER_SIDE = 3
+LEG_LENGTH = 60
+DIRECTION = 360 / 8
+ladybug.penup()
+ladybug.pensize(5)
+ladybug.color("black")
+ladybug.goto(0, -55)
+ladybug.pendown()
+
+count = 0
+# this loop makes the legs, set the direction, and moves forward
+while count < LEGS:
+    for i in range(LEGS_PER_SIDE):
+        ladybug.goto(0, -45)
+        if count <= 2:
+            ladybug.setheading(DIRECTION * count + 145)
+        else:
+            ladybug.setheading(DIRECTION * count + 165)
+        ladybug.forward(LEG_LENGTH)
+        count = count + 1
+
+ladybug.setheading(0)
+
 
 # and body
 ladybug.penup()
@@ -22,11 +52,13 @@ ladybug.pensize(2)
 ladybug.pendown()
 ladybug.forward(75)
 
+
 # config dots
 num_dots = 1
 xpos = -20
 ypos = -55
 ladybug.pensize(10)
+
 
 # draw two sets of dots
 while num_dots <= 2:
@@ -45,6 +77,7 @@ while num_dots <= 2:
     num_dots = num_dots + 1
 
 ladybug.hideturtle()
+
 
 wn = trtl.Screen()
 wn.mainloop()
