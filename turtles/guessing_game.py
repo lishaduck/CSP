8ee18b543444370fca52dbef6edbcc9baef7f3b4
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:41 -0600

    guessing game

---
+++ turtles/guessing_game.py
@@ -0,0 +1,17 @@
+"""Program: guessing_game - guess"""
+
+
+import random
+
+
+num = random.randint(0, 100)
+print(num)
+guess = int(input("Choose number from 0 through 100: "))
+
+
+while guess != num:
+    if guess < num:
+        guess = int(input("Too low :( Try again: "))
+    elif guess > num:
+        guess = int(input("Too high :( Try again: "))
+print("You got it!")
