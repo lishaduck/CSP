322721e148b112adb1511a3d03efa7fd8d3354e0
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:46 -0600

    update docstrings
    use correct import order

--- art/functions/func_practice_2_test.py
+++ art/functions/func_practice_2_test.py
@@ -1,60 +1,53 @@
 """Tests the func_practice_2.py module.
 """
+
+
 import unittest
-import func_practice_2
+
 import pytest
 
-# content of test_sample.py
+import func_practice_2
 
 
 class TripleTest(unittest.TestCase):
-    """This class contains tests.
-
-    It is used to remind me how tests are made.
-    """
+    """This class contains tests for the triple function."""
 
     @pytest.mark.task(taskno=1)
     def test_for_four(self):
-        """Tests that triple works."""
+        """Tests that triple works with 4 --> 12."""
         self.assertEqual(func_practice_2.triple(4), 12)
 
     @pytest.mark.task(taskno=2)
     def test_for_five(self):
-        """Tests that triple works."""
+        """Tests that triple works with 5 --> 15."""
         self.assertEqual(func_practice_2.triple(5), 15)
 
 
 class AreaTest(unittest.TestCase):
-    """This class contains tests.
-
-    It is used to remind me how tests are made.
-    """
+    """This class contains tests for the triangle_area function."""
 
     @pytest.mark.task(taskno=1)
     def test_for_four(self):
-        """Tests that triple works."""
+        """Tests that area works given 1 and 1."""
         self.assertEqual(func_practice_2.triangle_area(1, 1), 0.5)
 
     @pytest.mark.task(taskno=2)
     def test_for_five(self):
-        """Tests that triple works."""
+        """Tests that area works given 5 and 4."""
         self.assertEqual(func_practice_2.triangle_area(5, 4), 10)
 
 
 class MinTest(unittest.TestCase):
-    """This class contains tests.
-
-    It is used to remind me how tests are made.
-    """
+    """This class contains tests for the min_test."""
 
     @pytest.mark.task(taskno=1)
     def test_for_one(self):
-        """Tests that triple works."""
+        """Tests that min_val works given two equal values."""
         self.assertEqual(func_practice_2.min_val(1, 1), 1)
 
     @pytest.mark.task(taskno=2)
     def test_for_four(self):
-        """Tests that triple works."""
+        """Tests that min_val works given 5 and 4."""
         self.assertEqual(func_practice_2.min_val(5, 4), 4)
 
     @pytest.mark.task(taskno=3)
