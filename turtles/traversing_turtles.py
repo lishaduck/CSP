c544a9ead70c5ef81a858d3c0e8986930ae01003
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-09 07:55:41 -0600

    traversing turtles p. 2
    finish

--- turtles/traversing_turtles.py
+++ turtles/traversing_turtles.py
@@ -1,14 +1,20 @@
 """Program: traversing_turtles.py - Add code to make turtles move in a circle and change colors.
 """
+
+
 import turtle as trtl
+
 
 # create an empty list of turtles
 my_turtles = []
 
-# use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic","arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold","red", "blue", "green", "orange", "purple", "gold"]
+
+
+s: str
 for s in turtle_shapes:
     t = trtl.Turtle(shape=s)
     t.pencolor(turtle_colors[-1])
@@ -16,22 +22,37 @@ for s in turtle_shapes:
     turtle_colors.pop()
     my_turtles.append(t)
 
+
 # starts at 0,0
 startx = 0
 starty = 0
-direction = 0
 
-# loiops through all the turtles in the list of turtles
+
+# direction of the turtle
+direction = 0
+spin = 50
+
+
+# loops through all the turtles in the list of turtles
+t: trtl.Turtle
 for t in my_turtles:
     t.penup()
+
+    # set the direction via heading
     t.setheading(direction)
     t.goto(startx, starty)
     t.right(45)
-    t.forward(50)
+    t.forward(spin)
     t.pendown()
+
     # new x,y
     startx = t.xcor()
     starty = t.ycor()
+
+    # set direction so the next turtle will go a different way
     direction = t.heading()
+    spin = spin + 7
+
+
 wn = trtl.Screen()
 wn.mainloop()
