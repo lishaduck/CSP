fc4d15d38745802b353a9c5d4c8a00bef45134fc
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-11 12:18:21 -0600

    update sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -7,17 +7,57 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "CSP"
-copyright = "2022, lishaduck"
-author = "lishaduck"
+copyright = "2022, Elisha Dukes"
+author = "Elisha Dukes"
 release = "0.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.duration"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.duration",
+    "sphinx.ext.autosummary",
+    "myst_parser",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.coverage",
+    "sphinx.ext.todo",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosectionlabel",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
+autosectionlabel_prefix_document = True
+
+myst_enable_extensions = [
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "tasklist",
+]
+myst_heading_anchors = 3
+
+
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+    if not info["module"]:
+        return None
+    filename = info["module"].replace(".", "/")
+    return f"https://github.com/lishaduck/CSP/tree/master/{filename}.py"
 
 
 # -- Options for HTML output -------------------------------------------------
