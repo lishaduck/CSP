368c796a8bae7c7b60cd4446e41199c40274c4fd
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-14 15:29:25 -0600

    clean stuff up

--- csp/art/functions/catch-a-turtle/catch_a_turtle.py
+++ csp/art/functions/catch-a-turtle/catch_a_turtle.py
@@ -1,4 +1,4 @@
-"""Program catch_a_turtle.py - Its a game with a persistant state leaderboard.
+"""Program catch_a_turtle.py - It's a game with a persistant state leaderboard.
 
 Catch a turtle
 [Victor and Eli - 11.8-11]
@@ -38,9 +38,9 @@ score = 0
 
 
 # -----countdown variables-----
-font_setup = ("Arial", 20, "normal")
-timer = 5  # TODO: switch back to 30
-counter_interval = 1000  # 1000 represents 1 second
+FONT_SETUP = ("Arial", 20, "normal")
+timer = 30
+COUNTER_INTERVAL = 1000  # 1000 represents 1 second
 timer_up = False
 
 
@@ -53,40 +53,51 @@ counter.goto(-50, 250)
 
 
 # -----game functions-----
-def countdown():
+def countdown() -> None:
     """Create the timer and score."""
     global timer
     global timer_up
     counter.clear()
-    if timer <= 0:
-        counter.write(f"Time's Up!      Score: {score}", font=font_setup)
+    if timer > 0:
+        counter.write(f"Timer: {timer}  Score: {score}", font=FONT_SETUP)
+        timer -= 1
+        counter.getscreen().ontimer(countdown, COUNTER_INTERVAL)
+    else:
+        counter.write(f"Time's Up!      Score: {score}", font=FONT_SETUP)
         timer_up = True
+        print(f"Your score was: {score}!")
         manage_leaderboard()
         spot.hideturtle()
-    else:
-        counter.write(f"Timer: {timer}  Score: {score}", font=font_setup)
-        timer -= 1
-        counter.getscreen().ontimer(countdown, counter_interval)
 
 
 def spot_clicked(x: float, y: float):
+    """Event handler for the spot.
+
+    :param x: current x of the spot
+    :param y: current y of the spot
+    """
     if timer > 0:
-        update_score()
-        change_position()
+        global score
+        score += 1
+        print(score, end="\r")
+        change_position(x, y)
 
 
-def change_position():
+def change_position(x: float, y: float) -> None:
+    """Move the spot (called when spot_clicked).
+
+    :param x: current x of the spot
+    :param y: current y of the spot
+    """
     new_xpos = rand.randint(-200, 200)
     new_ypos = rand.randint(-150, 150)
+    while new_xpos == x:
+        new_xpos = rand.randint(-200, 200)
+    while new_ypos == y:
+        new_ypos = rand.randint(-200, 200)
     spot.penup()
     spot.goto(new_xpos, new_ypos)
     spot.pendown()
-
-
-def update_score():
-    global score
-    score = score + 1
-    print(score)
 
 
 # Game Code
@@ -100,11 +111,8 @@ spot.goto(-100, 0)
 
 
 # manages the leaderboard for top 5 scorers
-def manage_leaderboard():
-
-    global score
-    global spot
-
+def manage_leaderboard() -> None:
+    """Interface the leaderboard.py module."""
     # get the names and scores from the leaderboard file
     leader_names_list = lb.get_names(LEADERBOARD_FILE_NAME)
     leader_scores_list = lb.get_scores(LEADERBOARD_FILE_NAME)
@@ -118,13 +126,15 @@ def manage_leaderboard():
             player_name,
             score,
         )
-        lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
+        lb.draw_leaderboard(True, leader_names_list, leader_scores_list, counter, score)
 
     else:
-        lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
+        lb.draw_leaderboard(
+            False, leader_names_list, leader_scores_list, counter, score
+        )
 
 
 # ---------events----------
 spot.onclick(spot_clicked)
-wn.ontimer(countdown, counter_interval)
+wn.ontimer(countdown, COUNTER_INTERVAL)
 wn.mainloop()
--- csp/art/functions/catch-a-turtle/leaderboard.py
+++ csp/art/functions/catch-a-turtle/leaderboard.py
@@ -5,7 +5,6 @@ import csv
 import turtle as trtl
 from pathlib import Path
 
-Rank = trtl.Turtle()
 # set the levels of scoring
 MUD_SCORE = 5
 BRONZE_SCORE = 20
@@ -20,30 +19,18 @@ def get_names(file_name: Path) -> list[str]:
     :param file_name: A file name, in the format of a path.
     :return: A list of integers representing the scores.
     """
-    with file_name.open("r", encoding="utf-8", newline="") as leaderboard_file:
+    with file_name.open("rt", encoding="utf-8", newline="") as leaderboard_file:
 
         # use a for loop to iterate through the content of the file, one line at a time
         # note that each line in the file has the format "leader_name,leader_score" for example "Pat,50"
         names: list[str] = []
-        print("Getting names")
+        # print("Fetching names.")
         name_reader = csv.reader(leaderboard_file, dialect="excel")
         for line in name_reader:
-            leader_name = ""
-            index = 0
-
-            # use a while loop to read the leader name from the line (format is "leader_name,leader_score")
-            # TODO: fix IndexError
-            # File "/Users/dukese01/CSP/csp/art/functions/catch-a-turtle/leaderboard.py", line 35, in get_names
-            #   while line[index] != ",":
-            # IndexError: list index out of range
-            while line[index] != ",":
-                leader_name = leader_name + line[index]
-                index = index + 1
-            print(leader_name)
+            leader_name = line[0]
             # add the player name to the names list
             names.append(leader_name)
-        leaderboard_file.close()
-    print(names)
+            # print(names)
     #  return the names list in place of the empty list
     return names
 
@@ -55,32 +42,17 @@ def get_scores(file_name: Path) -> list[int]:
     :return: A list of integers representing the scores.
     """
     with file_name.open(
-        "r", encoding="utf-8"
+        "rt", encoding="utf-8"
     ) as leaderboard_file:  # be sure you have created this
 
         scores: list[int] = []
-        print("Getting scores.")
+        # print("Fetching scores.")
         score_reader = csv.reader(leaderboard_file, dialect="excel")
         for line in score_reader:
-            leader_score = ""
-            index = 0
-
-            # use a while loop to index beyond the comma, skipping the player's name
-            while line[index] != ",":
-                index = index + 1
-            # use a while loop to get the score
-            while line[index] != "\n":
-                leader_score = leader_score + line[index]
-                index = index + 1
-            leader_score = leader_score.replace(
-                ",", ""
-            )  # Source: https://www.delftstack.com/howto/python/remove-commas-from-string-in-python/
-
+            leader_score = int(line[1])
             # add the player score to the scores list
-            leader = int(leader_score)
-            scores.append(leader)
-        print(scores)
-        leaderboard_file.close()
+            scores.append(leader_score)
+        # print(scores)
     # return the scores in place of the empty list
     return scores
 
@@ -105,20 +77,17 @@ def update_leaderboard(
         else:
             index = index + 1
 
-    # TODO 12: store the latest leaderboard back in the file
+    # store the latest leaderboard back in the file
     with file_name.open(
-        "w",  # this mode opens the file and erases its contents for a fresh start
+        "wt",  # this mode opens the file and erases its contents for a fresh start
         encoding="utf-8",
         newline="",
     ) as leaderboard_file:
 
         leaderboard_writer = csv.writer(leaderboard_file, dialect="excel")
-        # TODO 13 loop through all the leaderboard elements and write them to the the file
-        for i in leaderboard_file:
-            leaderboard_writer.writerow(
-                leader_names[index] + "," + str(leader_scores[index])
-            )
-        leaderboard_file.close()
+        # loop through all the leaderboard elements and write them to the the file
+        for i, name in enumerate(leader_names):
+            leaderboard_writer.writerow([name, str(leader_scores[i])])
 
 
 def draw_leaderboard(
@@ -127,28 +96,21 @@ def draw_leaderboard(
     leader_scores: list[int],
     turtle_object: trtl.Turtle,
     player_score: int,
-) -> None:  # noqa: D300, D301
-    """Draw leaderboard and display a message to player.
+) -> None:
+    """Draw leaderboard and display a message to player."""
 
-    @LeParco1's version:
-    # def leaderboard():
-    #     \"\"\"Leaderboard\"\"\"
-    #     leaderboard = trtl.Turtle()
-    #     leaderboard.hideturtle()
-    #     leaderboard.write(f"Player Name:     Score:\n{name}     {score}")
-    """
     # clear the screen and move turtle object to (-200, 100) to start drawing the leaderboard
     font_setup = ("Arial", 20, "normal")
     turtle_object.clear()
     turtle_object.penup()
-    turtle_object.goto(-160, 100)
+    turtle_object.goto(-160, 200)
     turtle_object.hideturtle()
     turtle_object.down()
 
     # loop through the lists and use the same index to display the corresponding name and score, separated by a tab space '\t'
     for index, name in enumerate(leader_names):
         turtle_object.write(
-            str(index + 1) + "\t" + name + "\t" + str(name),
+            str(index + 1) + "\t" + str(name) + "\t" + str(leader_scores[index]),
             font=font_setup,
         )
         turtle_object.penup()
@@ -160,14 +122,14 @@ def draw_leaderboard(
     turtle_object.goto(-160, int(turtle_object.ycor()) - 50)
     turtle_object.pendown()
 
-    # TODO 14: display message about player making/not making leaderboard
+    # display message about player making/not making leaderboard
     if high_scorer:
-        if player_score > leader_scores[5]:
+        if player_score > leader_scores[4]:
             turtle_object.write(
                 "Congratulations!\nYou made the leaderboard!", font=font_setup
             )
-        elif player_score <= leader_scores[5]:
-            turtle_object.write("So close...")
+        elif player_score <= leader_scores[4]:
+            turtle_object.write("So close...", font=font_setup)
     else:
         turtle_object.write(
             "Sorry!\nYou didn't make the leaderboard.\nMaybe next time!",
@@ -179,8 +141,7 @@ def draw_leaderboard(
     turtle_object.goto(-160, int(turtle_object.ycor()) - 50)
     turtle_object.pendown()
 
-    # TODO 15: Display a gold/silver/bronze message if player earned a gold/silver/or bronze medal; display nothing if no medal
-    turtle_object.write(f"You caught the turtle {player_score} times.")
+    # Display a gold/silver/bronze message if player earned a gold/silver/or bronze medal; display nothing if no medal
     if MUD_SCORE <= player_score < BRONZE_SCORE:
         turtle_object.write(
             "You earned a dried piece of mud! It doesn't even resemble a medal... It's just a circle of mud. Oh, and you got a ruble, don't spend it all in one place.",
@@ -193,8 +154,14 @@ def draw_leaderboard(
     elif GOLD_SCORE <= player_score < PLATINUM_SCORE:
         turtle_object.write("You earned a gold medal!", font=font_setup)
     elif PLATINUM_SCORE <= player_score:
-        turtle_object.write("Poggers", font=font_setup)
+        turtle_object.write("Poggers?", font=font_setup)
     else:
         turtle_object.write(
             "Here's an Iranian Rial. You might as well spend it all in one place. Better Luck Next Time..."
         )
+
+    # move turtle to a new line
+    turtle_object.penup()
+    turtle_object.goto(-160, int(turtle_object.ycor()) - 50)
+    turtle_object.pendown()
+    turtle_object.write(f"You caught the turtle {player_score} times.", font=font_setup)
