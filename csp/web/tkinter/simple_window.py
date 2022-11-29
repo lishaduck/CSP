8e7b8bc79dc03e2abfdb2f615dd5c579eab34d68
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-29 15:44:53 -0600

    🏷️ update simple window

--- csp/web/tkinter/simple_window.py
+++ csp/web/tkinter/simple_window.py
@@ -1,41 +1,46 @@
 """A program creates a window on your screen using Tkinter."""
 
 
-from tkinter import *
+from tkinter import *  # pylint: disable=unused-wildcard-import, wildcard-import
 
-username = ""
-password = ""
+username: str = ""
+password: str = ""
 
 
 # main window
-root = Tk()
+root: Tk = Tk()
 root.title("Authorization")
 root.wm_geometry("400x200")
 
 # create empty frame
-frame_login = Frame(root)
+frame_login: Frame = Frame(root)
 frame_login.grid(row=0, column=0, sticky="news")
 
-lbl_username = Label(frame_login, text="Username:", font="Courier")
+lbl_username: Label = Label(frame_login, text="Username:", font="Courier")
 lbl_username.pack()
-ent_username = Entry(frame_login, bd=3)
+ent_username: Entry = Entry(frame_login, bd=3)
 ent_username.pack(pady=5)
 
-lbl_password = Label(frame_login, text="Password:", font="Courier")
+lbl_password: Label = Label(frame_login, text="Password:", font="Courier")
 lbl_password.pack()
-ent_password = Entry(frame_login, bd=3, show="*")
+ent_password: Entry = Entry(frame_login, bd=3, show="*")
 ent_password.pack(pady=5)
 
 
-def test_my_button():
-    global username
-    global password
+def _test_my_button() -> None:
+    """Set the username and password, then raise the frame_auth frame."""
+    global username  # pylint: disable=global-statement
+    global password  # pylint: disable=global-statement
     username = ent_username.get()
     password = ent_password.get()
     frame_auth.tkraise()
 
 
-def hello_there_foolish_mortal():
+def _hello_there_foolish_mortal() -> None:
+    """Print the username and password to the console.
+
+    Then, update and pack the user_label.
+    """
     print(
         "Hello there,",
         username + "!",
@@ -43,20 +48,22 @@ def hello_there_foolish_mortal():
         password + "! :)",
     )
     user_label.config(
-        text=("Hello, silly foolish one, you who is wonder, " + username + ".")
+        text=("Hello, silly foolish one, you who got in, " + username + ".")
     )
     user_label.pack()
 
 
-btn_login = Button(frame_login, text="Login", font="Courier", command=test_my_button)
+btn_login: Button = Button(
+    frame_login, text="Login", font="Courier", command=_test_my_button
+)
 btn_login.pack(pady=5)
 
 
-frame_auth = Frame(root)
+frame_auth: Frame = Frame(root)
 frame_auth.grid(row=0, column=0, sticky="news")
 
 
-user_label = Label(
+user_label: Label = Label(
     frame_auth,
     font="Courier",
     fg="yellow",
@@ -64,8 +71,8 @@ user_label = Label(
 )
 
 
-btn_color = Button(
-    frame_auth, text="Click Me, Child.", command=hello_there_foolish_mortal
+btn_color: Button = Button(
+    frame_auth, text="Click Me, Child.", command=_hello_there_foolish_mortal
 )
 btn_color.pack(pady=5)
 
