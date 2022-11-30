cfb3dca75d8a5204167f7b58a7bd849098e0939f
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-11-30 12:37:29 -0600

    finish day 2 work

--- csp/web/tkinter/my_first_gui/simple_window.py
+++ csp/web/tkinter/my_first_gui/simple_window.py
@@ -77,5 +77,63 @@ btn_color: Button = Button(
 btn_color.pack(pady=5)
 
 
+# Day 2 work starts here
+
+
+canvas: Canvas = Canvas(frame_auth, width=400, height=400)
+canvas.pack()
+
+
+# create an image using a gif file
+img2 = PhotoImage(file="strawberry-lemonade.png")
+# use image to create a canvas image
+myphoto = canvas.create_image(150, 150, image=img2)
+
+
+# create a circle
+circle = canvas.create_oval(100, 200, 130, 230, fill="red")
+# create a rectangle
+blue_rectangle = canvas.create_rectangle(50, 50, 70, 80, fill="blue")
+# create text
+screen_message = canvas.create_text(
+    300, 300, text="Welcome!", font=("Helvetica", 30), fill="black"
+)
+# create an image using a gif file
+img = PhotoImage(file="greenChar.gif")
+# use image to create a canvas image
+mychar = canvas.create_image(-100, -100, image=img)
+
+
+# This function is called when the arrow key is pressed
+def move(event: Event) -> object:
+    key = event.keysym
+    if key == "Right":
+        canvas.move(circle, 10, 0)  # change x positive
+    elif key == "Left":
+        canvas.move(circle, -10, 0)  # change x negative
+    if key == "Up":
+        canvas.move(blue_rectangle, 0, 10)  # change y positive
+    elif key == "Down":
+        canvas.move(blue_rectangle, 0, -10)  # change y negative
+
+
+def move_character(event: Event) -> object:
+    canvas.coords(mychar, event.x, event.y)
+
+
+# bind the function to the mouse click
+canvas.bind_all("<Button-1>", move_character)
+
+
+# bind keyboard input to move_circle
+canvas.bind_all("<Key>", move)
+
+
+# Figure out how to make a triangle (or any POLYGON) on the canvas
+triangle = canvas.create_polygon(10, 10, 10, 60, 50, 35, fill="red")
+
+# end Day 2 work
+
+
 frame_login.tkraise()
 root.mainloop()
