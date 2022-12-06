60550abee967a3eee517a4ce1d5a1843da0ff580
lishaduck <88557639+lishaduck@users.noreply.github.com>
2022-12-06 12:09:09 -0600

    fix: spacing

--- csp/web/tkinter/candy_monster/candy_monster.py
+++ csp/web/tkinter/candy_monster/candy_monster.py
@@ -1,29 +1,65 @@
-# YOUR NAME HERE
-# The Candy Monster Game
+""" The Candy Monster Game
+
+Victor, Eli
+"""
+
+
 # import statements
+
+
 # Step 1 - Create the GUI
+
 # create window and canvas
+
 # create game title and instructions text objects
+
 # create score display as label widget
+
 # Level widget
+
 # create image using green frog
+
+
 # Step 2: Add Code to Make Candy and Drop It
+
 # variable declarations for managing candy
+
 # FUNCTION SECTION
+
 # function to make the candies
+
 # function to move the candies downward
+
+
 # Step 3: Add code to update the score and end the game
+
 # function that will update score, level and candy_speed
+
 # function to end the game
+
+
 # Step 4: Add code to check if candy and character collide
+
 # Collision function to check distance between objects
+
 # Check hits function to see if character hits a bad candy
+
+
 # Step 5:Add code to control the character with arrow keys
+
 # Direction variable
+
 # Function to handle when user presses arrow keys
+
 # Function to stop movement when user does not press arrow keys
+
 # Function to move character
+
+
 # Bind the keys to the character
+
 # Step 6: Schedule all the functions and make a working game!
+
 # Schedule all of the functions
+
 # last line of code
