d771011dd0e21ff0e472f1735ad5c0ae21f943c8
Eli <88557639+lishaduck@users.noreply.github.com>
2023-01-10 19:17:23 -0600

    feat: make an actual test

--- tests/ap_2023/test_main.py
+++ tests/ap_2023/test_main.py
@@ -2,12 +2,23 @@
 
 
 from src.ap_2023 import main
+import tkinter as tk
 
+import pytest
 
 # TODO: add actual test
 class TestMain:
     """This class contains tests."""
 
-    def test_default(self):
+    @pytest.fixture()
+    def make_window(self):
+        root = main.App()
+        yield root
+
+    @pytest.fixture()
+    def define_tk(self):
+        return tk.Tk
+
+    def test_default(self, make_window, define_tk):
         """Tests that the --default CLI option works."""
-        assert main.App != main
+        assert isinstance(make_window, define_tk)
